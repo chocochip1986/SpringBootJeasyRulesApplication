5b13e426ff6a409b01b9dc59938036cebe252397
Ou Guozheng <abc@abc.com>
2021-07-15 09:46:19 +0800

    stuff

--- bespoke.rules.engine/src/main/java/bespoke/controllers/SimpleController.java
+++ bespoke.rules.engine/src/main/java/bespoke/controllers/SimpleController.java
@@ -1,5 +1,7 @@
 package bespoke.controllers;
 
+import bespoke.services.RuleEngineService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+    @Autowired private RuleEngineService ruleEngineService;
     @GetMapping()
     public ResponseEntity<String> trigger() {
+        ruleEngineService.init();
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
